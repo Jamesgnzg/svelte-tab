b470a0c47c8120a5519bf68aabb667c79b72571d
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-07-14 10:23:33 +0800

    Add borders on selection and hover

--- src/App.svelte
+++ src/App.svelte
@@ -31,15 +31,17 @@
 </script>
 
 <main class="p-5">
-  <ul class="flex flex-wrap pl-0 mb-0 list-none">
+  <ul class="flex flex-wrap pl-0 list-none border-b-1 border-[#dee2e6] rounded-md">
     {#each tabItems as items}
-      <li>
+      <li class={activeTab === items.value ? `border rounded-md border-b-white border-[#dee2e6] bg-white text-[#495057] -mb-[1px]` : ''}>
         <!-- svelte-ignore a11y_click_events_have_key_events -->
-        <span class="p-4 cursor-pointer" onclick={setActiveTab(items.value)} role="button" tabindex={items.value} aria-label={items.title}>
+        <span class={`p-4 cursor-pointer block border border-transparent rounded-md ${activeTab !== items.value ? `hover:border-[#dee2e6] hover:border-b-white` : ''}`} onclick={setActiveTab(items.value)} role="button" tabindex={items.value} aria-label={items.title}>
           {items.title}
         </span>
       </li>
     {/each}
   </ul>
-  <ActiveContent />
+  <div class="p-5 border rounded-md border-t-0 border-[#dee2e6] min-h-screen">
+      <ActiveContent />
+  </div>
 </main>
